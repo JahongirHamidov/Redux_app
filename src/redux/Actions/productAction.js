b4776770cab88165f259d4bbc0d510b6b6fe2ba0
Jahongir <hamidovjahongir444@gmail.com>
2021-11-03 17:28:06 +0500

    project added github

---
+++ src/redux/Actions/productAction.js
@@ -0,0 +1,27 @@
+import {
+    SET_PRODUCTS,
+    SELECTED_PRODUCT,
+    REMOVE_SELECTED_PRODUCT
+} from '../Constants/action-types'
+
+export const setProducts = (products) => {
+    return {
+        type: SET_PRODUCTS,
+        payload: products
+    }
+}
+
+export const selectedProducts = (products) => {
+    return {
+        type: SELECTED_PRODUCT,
+        payload: products
+    }
+}
+
+export const removeSelectedProducts = (products) => {
+    return {
+        type: REMOVE_SELECTED_PRODUCT
+    }
+}
+ 
+
---
+++ src/redux/Reducers/productReducer.js
@@ -0,0 +1,31 @@
+import {actionTypes} from '../Constants/action-types'
+const {
+    SET_PRODUCTS,
+    // SELECTED_PRODUCT,
+    //REMOVE_SELECTED_PRODUCT
+} = actionTypes
+
+const initialState = {
+    products: [
+        {
+            id:1,
+            title: "Jahongir",
+            category: "programmer"
+        }
+    ]
+}
+export const productReducer = (state, {type, payload}) => {
+    switch (type) {
+        case SET_PRODUCTS:
+            return state
+            
+  //      case SELECTED_PRODUCT:
+
+            
+//        case REMOVE_SELECTED_PRODUCT:
+
+    
+        default:
+            return state;
+    }
+}
