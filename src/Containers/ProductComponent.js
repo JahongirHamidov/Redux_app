be343cbab94b3362dd5920cd49c6a30b57578c38
Jahongir <hamidovjahongir444@gmail.com>
2021-11-06 21:02:12 +0500

    material ui added

--- src/Containers/ProductComponent.js
+++ src/Containers/ProductComponent.js
@@ -1,11 +1,64 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
+
+import { makeStyles } from '@material-ui/core/styles'
+
+
+import {
+    Card,
+    Button,
+    CardMedia,
+    Typography,
+    CardActions,
+    CardContent,
+    CardActionArea,
+} from '@material-ui/core'
+
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+})
 
 const ProductComponent = () => {
+        const products = useSelector( state => state.productReducer.products)
+        const { id, title } =products[0]
+
+        const classes = useStyles()
     return (
-        <div>
-            <h1>Product Component</h1>
-        </div>
+        <Card className={classes.root}>
+            <CardActionArea>
+                <CardMedia
+                className={classes.media}
+                image="/static/images/cards/contemplative-reptile.jpg"
+                title="Contemplative Reptile"
+                />
+                <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                    {title}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {title}
+                </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                Share
+                </Button>
+                <Button size="small" color="primary">
+                Learn More
+                </Button>
+            </CardActions>
+        </Card>
     )
 }
+
+
 
 export default ProductComponent
